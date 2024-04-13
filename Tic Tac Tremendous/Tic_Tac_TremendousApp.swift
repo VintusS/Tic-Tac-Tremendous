476e86698db0d257341dc957679403aae71ab09c
VintusS <dragomir.mindrescu@isa.utm.md>
2024-04-13 16:58:40 +0300

    Initial Commit

---
+++ Tic Tac Tremendous/Tic_Tac_TremendousApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_TremendousApp.swift
+//  Tic Tac Tremendous
+//
+//  Created by Dragomir Mindrescu on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_TremendousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
